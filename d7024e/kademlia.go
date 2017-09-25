bd6dc8ab0ce3494fdb95f2c8453ceb0677f547ab
Joey Ong <Joey@pub95-188.pub.ltu.se>
2017-09-25 17:30:24 +0200

    lookup

--- d7024e/kademlia.go
+++ d7024e/kademlia.go
@@ -1,10 +1,26 @@
 package d7024e
 
+import (
+	"fmt"
+)
+
 type Kademlia struct {
+	//network Network
+	rt *RoutingTable
 }
 
+func NewKademlia (me *Contact) *Kademlia {
+	rt := NewRoutingTable(me)
+	return Kademlia{rt}
+}
+
+const alpha = 3
+const intk = 20
+
 func (kademlia *Kademlia) LookupContact(target *Contact) {
-	// TODO
+	contacts := kademlia.rt.FindClosestContacts(target.ID, intk)
+
+
 }
 
 func (kademlia *Kademlia) LookupData(hash string) {
